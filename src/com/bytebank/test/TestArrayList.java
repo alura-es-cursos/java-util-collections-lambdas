a5e44b29b4b75f0b3c1a2c0e766bf7b6bee6ca2c
Diego Rojas <darguelles.rojas91@gmail.com>
2021-02-21 23:56:51 -0300

    Clase 5

--- src/com/bytebank/test/TestArrayList.java
+++ src/com/bytebank/test/TestArrayList.java
@@ -1,6 +1,7 @@
 package com.bytebank.test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
@@ -14,11 +15,18 @@ public class TestArrayList {
 	public static void main(String[] args) {
 		
 		// <> Forzando a que acepte solo un tipo de objeto
-//		List<Cliente> listaCLientes = new LinkedList<>();
 		
+		// LinkedList funciona con otra estructura de datos
+		// List<Cliente> listaCLientes = new LinkedList<>();
+		
+		// El Vector es thread safe, entonces crea una unica instancia 
+		// para todas las pilas de ejecucion
+		// List<Cuenta> lista = new Vector<Cuenta>();
 		
 		// Obligatorio especificar        No es obligatorio
-		List<Cuenta> lista = new Vector<Cuenta>();
+		
+		List<Cuenta> lista = new ArrayList<Cuenta>();
+
 		// referencia    Objeto -> HEAP
 		Cuenta cc = new CuentaCorriente(11, 22);
 		Cuenta cc2 = new CuentaCorriente(13, 42);
@@ -43,7 +51,7 @@ public class TestArrayList {
 		}
 		
 		boolean contiene = lista.contains(cc3);
-		// Por referencia
+		// Por valor
 		if (contiene) {
 			System.out.println("Si, es igual (equals)");
 		}
